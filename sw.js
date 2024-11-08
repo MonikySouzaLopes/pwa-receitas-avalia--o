a89c79213acfa1afc91392b283f1b5cd76cf0cc6
Moniky <monikysouzalopes@gmail.com>
2024-11-08 00:11:36 -0300

    atualizado dia 07.11

--- sw.js
+++ sw.js
@@ -1,21 +1,27 @@
-let cacheName = "receitas-pwa";
-let filesToCache = ["/", "index.html", "carnes.html", "doces.html",
+const cacheName = "receitas-pwa-v2";
+const filesToCache = ["/", "index.html", "carnes.html", "doces.html",
     "massas.html", "/js/main.js", "/css/styleCarne.css", "/css/style.css", 
     "/css/styleDoce.css", "/css/styleMassa.css"
 ];
 
 self.addEventListener("install", (e) => {
     e.waitUntil(
-        caches.open(cacheName).then(function (cache){
+        caches.open(cacheName).then((cache) => {
             return cache.addAll(filesToCache);
         })
     );
 });
 
-self.addEventListener("fetch", (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response || fetch(e.request);
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((name) => {
+                    if (name !== cacheName) {
+                        return caches.delete(name);
+                    }
+                })
+            );
         })
     );
 });
